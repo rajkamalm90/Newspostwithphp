ea9e9d2f385185becd1fbe816d9c5a788a72d417
Rajkamal <119285566+rajkamalm90@users.noreply.github.com>
2023-08-20 01:53:42 +0530

    Add files via upload

---
+++ Navbar/left-nav.php
@@ -0,0 +1,13 @@
+<div class="col-md-3 side">
+		    <div class="list-group">
+			  <a href="#" class="list-group-item active">
+				<i class="fa fa-tachometer" aria-hidden="true"> Dashboard</i>
+			  </a>
+			  
+			  <a href="userdeta.php" class="list-group-item"> <span class="badge">20</span>User Data</a>
+			  <a href="deta.php" class="list-group-item"> <span class="badge">14</span>News Detail</a>
+			  <a href="category-data.php" class="list-group-item"> <span class="badge">124</span>Category Details</a>
+			  <a href="#" class="list-group-item"> <span class="badge">124</span>Visitors</a>
+			  <a href="#" class="list-group-item"> <span class="badge">Active</span>Logout</a>
+            </div>
+		 </div>
---
+++ Navbar/nav.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><i class="fa fa-dot-circle-o" aria-hidden="true"> Branded</i></a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+     
+	   <ul class="nav navbar-nav navbar-right">
+      <li><a href="index.php"><i class="fa fa-home" aria-hidden="true"> HOME</a></i></li>
+
+        <li><a href="add-users.php"><i class="fa fa-puzzle-piece" aria-hidden="true"> Add-Users</a></i></li>
+		<li><a href="Add State.php"><i class="fa fa-puzzle-piece" aria-hidden="true"> Add State</a></i></li>
+		<li><a href="Add Writers.php"><i class="fa fa-puzzle-piece" aria-hidden="true"> Add Writers</a></i></li>
+		<li><a href="add-category.php"><i class="fa fa-puzzle-piece" aria-hidden="true"> Add Category</a></i></li>
+		<li><a href="#"><i class="fa fa-info-circle" aria-hidden="true"></a></i></li>
+		<li><a href="form.php"><i class="fa fa-clipbord" aria-hidden="true"> Add-Posts</a></i></li>
+		<li><a href="#"><i class="fa fa-compress" aria-hidden="true"></a></i></li>
+		<li><a href="logout.php"><i class="fa fa-sign-in" aria-hidden="true"> Logout</a></i></li>
+		
+      </ul>
+	  
+    </div>
+  </div>
+</nav>
